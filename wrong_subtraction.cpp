6b5ab43738ac0b0a07e99afb85997253b679439d
Chaman Sarkar <84088334+Chaman1998@users.noreply.github.com>
2021-09-21 03:06:35 +0530

    Update wrong_subtraction.cpp

--- wrong_subtraction.cpp
+++ wrong_subtraction.cpp
@@ -1,3 +1,14 @@
+// Examples
+// input
+// 512 4
+// output
+// 50
+// input
+// 1000000000 9
+// output
+// 1
+// Note
+// The first example corresponds to the following sequence: 512→511→510→51→50.
 #include<iostream>
 using namespace std;
 int main()
